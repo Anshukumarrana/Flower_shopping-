6335257d38b5853872232106411b4b9b5a7c500f
Anshukumarrana <ranaanshu7763@gmail.com>
2025-03-01 22:44:34 +0530

    Add files via upload

---
+++ flower.js
@@ -0,0 +1,356 @@
+const addToCartBtn = document.getElementById('addToCartBtn');
+const cartModal = document.getElementById('cartModal');
+const closeModalBtn = document.getElementById('closeModalBtn');
+const cartForm = document.getElementById('cartForm');
+
+
+addToCartBtn.addEventListener('click', function() {
+  cartModal.style.display = 'block';
+});
+
+
+closeModalBtn.addEventListener('click', function() {
+  cartModal.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModal) {
+    cartModal.style.display = 'none';
+  }
+});
+
+
+cartForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantity = document.getElementById('quantity').value;
+  const Address = document.getElementById('Address').value;
+  alert("Order Successful");
+  cartModal.style.display = 'none'; 
+});
+/*No.2 */
+const addToCartBtna = document.getElementById('addToCartBtna');
+const cartModala = document.getElementById('cartModala');
+const closeModalBtna = document.getElementById('closeModalBtna');
+const cartForma = document.getElementById('cartForma');
+
+
+addToCartBtna.addEventListener('click', function() {
+  cartModala.style.display = 'block';
+});
+
+
+closeModalBtna.addEventListener('click', function() {
+  cartModala.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModala) {
+    cartModala.style.display = 'none';
+  }
+});
+
+
+cartForma.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantitya = document.getElementById('quantitya').value;
+  const Addressa = document.getElementById('Addressa').value;
+  alert("Order Successful");
+  cartModala.style.display = 'none'; 
+});
+
+/*No.4 */
+const addToCartBtnc = document.getElementById('addToCartBtnc');
+const cartModalc = document.getElementById('cartModalc');
+const closeModalBtnc = document.getElementById('closeModalBtnc');
+const cartFormc = document.getElementById('cartFormc');
+
+
+addToCartBtnc.addEventListener('click', function() {
+  cartModalc.style.display = 'block';
+});
+
+
+closeModalBtnc.addEventListener('click', function() {
+  cartModalc.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModalc) {
+    cartModalc.style.display = 'none';
+  }
+});
+
+
+
+cartFormc.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantityc = document.getElementById('quantityc').value;
+  const Addressc = document.getElementById('Addressc').value;
+  alert("Order Successful");
+  cartModalc.style.display = 'none'; 
+});
+
+
+
+/*3*/
+const addToCartBtnb = document.getElementById('addToCartBtnb');
+const cartModalb = document.getElementById('cartModalb');
+const closeModalBtnb = document.getElementById('closeModalBtnb');
+const cartFormb = document.getElementById('cartFormb');
+
+
+addToCartBtnb.addEventListener('click', function() {
+  cartModalb.style.display = 'block';
+});
+
+
+closeModalBtnb.addEventListener('click', function() {
+  cartModalb.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModalb) {
+    cartModalb.style.display = 'none';
+  }
+});
+
+
+
+cartFormb.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantityb = document.getElementById('quantityb').value;
+  const Addressb = document.getElementById('Addressb').value;
+  alert("Order Successful");
+  cartModalb.style.display = 'none'; 
+});
+/*4*/
+const addToCartBtnd = document.getElementById('addToCartBtnd');
+const cartModald = document.getElementById('cartModald');
+const closeModalBtnd = document.getElementById('closeModalBtnd');
+const cartFormd = document.getElementById('cartFormd');
+
+
+addToCartBtnd.addEventListener('click', function() {
+  cartModald.style.display = 'block';
+});
+
+
+closeModalBtnd.addEventListener('click', function() {
+  cartModald.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModald) {
+    cartModald.style.display = 'none';
+  }
+});
+
+
+
+cartFormd.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantityd = document.getElementById('quantityd').value;
+  const Addressd = document.getElementById('Addressd').value;
+  alert("Order Successful");
+  cartModald.style.display = 'none'; 
+});
+
+/*step 2 */
+/*6 */
+const addToCartBtne = document.getElementById('addToCartBtne');
+const cartModale = document.getElementById('cartModale');
+const closeModalBtne = document.getElementById('closeModalBtne');
+const cartForme = document.getElementById('cartForme');
+
+
+addToCartBtne.addEventListener('click', function() {
+  cartModale.style.display = 'block';
+});
+
+
+closeModalBtne.addEventListener('click', function() {
+  cartModale.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModale) {
+    cartModale.style.display = 'none';
+  }
+});
+
+
+cartForme.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantitye = document.getElementById('quantitye').value;
+  const Addresse = document.getElementById('Addresse').value;
+  alert("Order Successful");
+  cartModal.style.display = 'none'; 
+});
+/*No.7 */
+const addToCartBtnf = document.getElementById('addToCartBtnf');
+const cartModalf = document.getElementById('cartModalf');
+const closeModalBtnf = document.getElementById('closeModalBtnf');
+const cartFormf = document.getElementById('cartFormf');
+
+
+addToCartBtnf.addEventListener('click', function() {
+  cartModalf.style.display = 'block';
+});
+
+
+closeModalBtnf.addEventListener('click', function() {
+  cartModalf.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModalf) {
+    cartModalf.style.display = 'none';
+  }
+});
+
+
+cartFormf.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantityf = document.getElementById('quantityf').value;
+  const Addressf = document.getElementById('Addressf').value;
+  alert("Order Successful");
+  cartModala.style.display = 'none'; 
+});
+
+/*No.8 */
+const addToCartBtng = document.getElementById('addToCartBtng');
+const cartModalg = document.getElementById('cartModalg');
+const closeModalBtng = document.getElementById('closeModalBtng');
+const cartFormg = document.getElementById('cartFormg');
+
+
+addToCartBtng.addEventListener('click', function() {
+  cartModalg.style.display = 'block';
+});
+
+
+closeModalBtng.addEventListener('click', function() {
+  cartModalg.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModalg) {
+    cartModalg.style.display = 'none';
+  }
+});
+
+
+
+cartFormg.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantityg = document.getElementById('quantityg').value;
+  const Addressg = document.getElementById('Addressg').value;
+  alert("Order Successful");
+  cartModalc.style.display = 'none'; 
+});
+
+
+
+/*8*/
+const addToCartBtnh = document.getElementById('addToCartBtnh');
+const cartModalh = document.getElementById('cartModalh');
+const closeModalBtnh = document.getElementById('closeModalBtnh');
+const cartFormh = document.getElementById('cartFormh');
+
+
+addToCartBtnh.addEventListener('click', function() {
+  cartModalh.style.display = 'block';
+});
+
+
+closeModalBtnh.addEventListener('click', function() {
+  cartModalh.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModalh) {
+    cartModalh.style.display = 'none';
+  }
+});
+
+
+
+cartFormh.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantityh = document.getElementById('quantityh').value;
+  const Addressh = document.getElementById('Addressh').value;
+  alert("Order Successful");
+  cartModalb.style.display = 'none'; 
+});
+/*10*/
+const addToCartBtni = document.getElementById('addToCartBtni');
+const cartModali = document.getElementById('cartModali');
+const closeModalBtni = document.getElementById('closeModalBtni');
+const cartFormi = document.getElementById('cartFormi');
+
+
+addToCartBtni.addEventListener('click', function() {
+  cartModali.style.display = 'block';
+});
+
+
+closeModalBtni.addEventListener('click', function() {
+  cartModali.style.display = 'none';
+});
+
+
+window.addEventListener('click', function(event) {
+  if (event.target === cartModali) {
+    cartModali.style.display = 'none';
+  }
+});
+
+
+
+cartFormi.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const quantityi = document.getElementById('quantityi').value;
+  const Addressi = document.getElementById('Addressi').value;
+  alert("Order Successful");
+  cartModald.style.display = 'none'; 
+});
+
+document.getElementById('instaButton').addEventListener('click', function() {
+  window.open('https://www.instagram.com', '_blank');
+});
+
+document.getElementById('Facebook').addEventListener('click', function() {
+  window.open('https://www.facebook.com', '_blank');
+});
+
+document.getElementById('com').addEventListener('click', function() {
+  window.open('https://x.com/i/flow/login', '_blank');
+});
+
+document.getElementById('rupay').addEventListener('click', function() {
+  window.open('https://www.rupay.co.in', '_blank');
+});
+document.getElementById('developer').addEventListener('click', function() {
+  window.open('https://developer.mastercard.com/open-banking-us/documentation/connect/generate-2-connect-url-apis/', '_blank');
+});
+document.getElementById('phonepay').addEventListener('click', function() {
+  window.open('https://www.phonepe.com/hi/index.html', '_blank');
+});
+document.getElementById('visa').addEventListener('click', function() {
+  window.open('https://indianvisaonline.gov.in/evisa/tvoa.html', '_blank');
+});
+document.getElementById('pay').addEventListener('click', function() {
+  window.open('https://support.google.com/googlepay/answer/7644068?hl=en', '_blank');
+});
+document.getElementById('paytm').addEventListener('click', function() {
+  window.open('https://business.paytm.com/docs/v1/payment-link/', '_blank');
+});
