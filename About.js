de6e886746c4a45b120c11be92e0f548f6927714
Anshukumarrana <ranaanshu7763@gmail.com>
2025-03-01 22:45:45 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,31 @@
+   
+document.getElementById('instaButton').addEventListener('click', function() {
+    window.open('https://www.instagram.com', '_blank');
+  });
+  
+  document.getElementById('Facebook').addEventListener('click', function() {
+    window.open('https://www.facebook.com', '_blank');
+  });
+  
+  document.getElementById('com').addEventListener('click', function() {
+    window.open('https://x.com/i/flow/login', '_blank');
+  });
+  
+  document.getElementById('rupay').addEventListener('click', function() {
+    window.open('https://www.rupay.co.in', '_blank');
+  });
+  document.getElementById('developer').addEventListener('click', function() {
+    window.open('https://developer.mastercard.com/open-banking-us/documentation/connect/generate-2-connect-url-apis/', '_blank');
+  });
+  document.getElementById('phonepay').addEventListener('click', function() {
+    window.open('https://www.phonepe.com/hi/index.html', '_blank');
+  });
+  document.getElementById('visa').addEventListener('click', function() {
+    window.open('https://indianvisaonline.gov.in/evisa/tvoa.html', '_blank');
+  });
+  document.getElementById('pay').addEventListener('click', function() {
+    window.open('https://support.google.com/googlepay/answer/7644068?hl=en', '_blank');
+  });
+  document.getElementById('paytm').addEventListener('click', function() {
+    window.open('https://business.paytm.com/docs/v1/payment-link/', '_blank');
+  });
